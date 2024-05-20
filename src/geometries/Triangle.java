f2c53faa351a9a3bf0fad8114e558c62c04853eb
YafitA <YAFITTA5@GMAIL.COM>
2024-05-20 17:36:05 +0300

    Fixes and changes for stage 1 and start of stage 2

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -8,9 +8,10 @@ import primitives.Point;
 public class Triangle extends Polygon {
 
     /**
-     * Constructs triangle object with the specified list of points
-     *
-     * @param vertices list of vertices according to their order by edge path
+     * Constructs triangle object with the 3 points
+     * @param p1 the 1sd point
+     * @param p2 the 2sd point
+     * @param p3 the 3sd point
      */
     public Triangle(Point p1, Point p2, Point p3) {
         super(new Point[]{p1, p2, p3});
