259984f3be3e6380450dbd7f967ac6b98939f8aa
YafitA <YAFITTA5@GMAIL.COM>
2024-05-22 21:39:49 +0300

    unittests vector done

--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -2,10 +2,8 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
-import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static primitives.Util.isZero;
+
 
 /**
  * Unit tests for primitives.Vector class
@@ -99,12 +97,13 @@ public class VectorTest {
     @Test
     public void testScale() {
         Vector v1 = new Vector(1, 2, 3);
-        Vector v1Opposite = new Vector(-1, -2, -3);
         Vector v2 = new Vector(-2, -4, -6);
-        Vector v3 = new Vector(0, 3, -2);
-        Vector v4 = new Vector(1, 2, 2);
         /// ============ Equivalence Partitions Tests ==============
+        // TC01: test scale result
+        assertEquals(v1.scale(-2), v2, "ERROR: Scale() wrong value");
         // =============== Boundary Values Tests ==================
+        // TC01: test scale result equal zero vector
+        assertThrows(IllegalArgumentException.class, ()->v1.scale(0), "ERROR: zero vector does not throw an exception");
     }
 
     /**
@@ -133,7 +132,6 @@ public class VectorTest {
         Vector v1 = new Vector(1, 2, 3);
         Vector v2 = new Vector(-2, -4, -6);
         Vector v3 = new Vector(0, 3, -2);
-        //
 
         /// ============ Equivalence Partitions Tests ==============
         //TC01: test Cross-Product result
@@ -157,7 +155,19 @@ public class VectorTest {
      */
     @Test
     public void testNormalize() {
-        /// ============ Equivalence Partitions Tests ==============
-        // =============== Boundary Values Tests ==================
+
+        /// ============ Equivalence Partitions Tests =============
+        Vector v = new Vector(1, 2, 3);
+        Vector u = v.normalize();
+        //TC01: test vector normalization vs vector length and cross-product
+        assertEquals(1, u.length(), DELTA, "ERROR: the normalized vector is not a unit vector");
+
+        //TC02: test that the vectors are co-lined
+        assertThrows(IllegalAccessError.class, ()->v.crossProduct(u),
+                "ERROR: the normalized vector is not parallel to the original one");
+
+        //TC03: test that the normalized vector is not opposite to the original one
+        assertFalse(v.dotProduct(u) < 0, "ERROR: the normalized vector is opposite to the original one");
+
     }
 }
