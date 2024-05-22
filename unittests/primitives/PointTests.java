945912ae41c9d09acefcfcd3f13facd10617e5c3
YafitA <YAFITTA5@GMAIL.COM>
2024-05-22 10:05:52 +0300

    unittests continuation

---
+++ unittests/primitives/PointTests.java
@@ -0,0 +1,40 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ */
+
+public class PointTests {
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
+    @Test
+    public void testSubtract() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
+    @Test
+    public void testDistance() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
+    @Test
+    public void testDistanceSquared() {
+    }
+}
---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,60 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Vector class
+ */
+public class VectorTest {
+
+    /**
+     * Test method for {@link primitives.Vector#lengthSquared()}.
+     */
+    @Test
+    public void testLengthSquared() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#length()}.
+     */
+    @Test
+    public void testLength() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#add(primitives.Vector)}.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#scale(double)}.
+     */
+    @Test
+    public void testScale() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#dotProduct(primitives.Vector)}.
+     */
+    @Test
+    public void testDotProduct() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
+    @Test
+    public void testCrossProduct() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#normalize()}.
+     */
+    @Test
+    public void testNormalize() {
+    }
+}
