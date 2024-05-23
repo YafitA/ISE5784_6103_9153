8ccf8deb4736b7d954f99b21caa677ea7537f3c0
Tamar <trothste@g.jct.ac.il>
2024-05-23 18:32:11 +0300

    finished pointTest class

--- unittests/primitives/PointTests.java
+++ unittests/primitives/PointTests.java
@@ -10,46 +10,79 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Unit tests for primitives.Point class
  */
 
-public class PointTests {
+class PointTests {
 
-    private final double DELTA = 0.000001;
+    /**
+     * First point for tests
+     */
+    private static final Point p1 = new Point(1, 2, 3);
+    /**
+     * Second point for tests
+     */
+    private static final Point p2 = new Point(2 ,4, 6);
+    /**
+     * Third point for tests
+     */
+    private static final Point p3 = new Point(2, 4, 5);
+    /**
+     * Vector for tests
+     */
+    private static final Vector v1 = new Vector(1, 2, 3);
+    /**
+     * Delta value for accuracy when comparing the numbers of type 'double' in assertEquals
+     */
+    private static final double DELTA = 0.00001;
+
 
     /**
      * Test method for {@link primitives.Point#add(primitives.Vector)}.
      */
     @Test
-    public void testAdd() {
+    void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Adds vector v1 to p1
+        assertEquals(p1.add(v1), p2, "ERROR: (point1 + vector) != point2 and does not work correctly");
     }
 
     /**
      * Test method for {@link primitives.Point#subtract(primitives.Point)}.
      */
     @Test
-    public void testSubtract() {
-        Point p1 = new Point(1, 2, 3);
-        Point p2 = new Point(2, 4, 6);
-        Vector v1 = new Vector(1, 2, 3);
+    void testSubtract() {
         // ============ Equivalence Partitions Tests ==============
-        // TC01: subs
-        assertEquals(p2.subtract(p1), v1,
-                "ERROR: (point2 - point1) does not work correctly");
-        // =============== Boundary Values Tests ==================
+        // TC01: subtracts p1 from p2
+        assertEquals(p2.subtract(p1), v1, "ERROR: (point2 - point1) does not work correctly");
 
-        p1.subtract(p1);
-        out.println("ERROR: (point - itself) does not throw an exception");
+        // =============== Boundary Values Tests ==================
+        // TC01: subtracts point from itself
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1), "ERROR: (point - itself) does not throw an exception");
     }
 
     /**
      * Test method for {@link primitives.Point#distance(primitives.Point)}.
      */
     @Test
-    public void testDistance() {
+    void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: distance between p1 to p3
+        assertEquals(3, p1.distance(p3), DELTA, "distance between points is wrong");
+
+        // =============== Boundary Values Tests ==================
+        // TC01: distance between p1 to p1
+        assertEquals(0, p1.distance(p1), DELTA, "point distance to itself is not zero");
     }
 
     /**
      * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
      */
     @Test
-    public void testDistanceSquared() {
+    void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: distance squared between p1 to p3
+        assertEquals(9, p1.distanceSquared(p3), DELTA, "squared distance between points is wrong");
+
+        // =============== Boundary Values Tests ==================
+        // TC01: distance squared between p1 to p1
+        assertEquals(0, p1.distanceSquared(p1), DELTA, "point distance to itself is not zero");
     }
 }
