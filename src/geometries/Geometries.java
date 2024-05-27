57807cc070abaed3be1b74fe9547495cbc8eed03
YafitA <YAFITTA5@GMAIL.COM>
2024-05-27 17:36:52 +0300

    add geo tests

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -1,23 +1,38 @@
 package geometries;
 
+import primitives.Point;
+import primitives.Ray;
+
 import java.util.LinkedList;
 import java.util.List;
 
-public class Geometries extends Intersectable {
+public class Geometries implements Intersectable {
 
     private final List<Intersectable> L = new LinkedList<>();
 
     /**
-     * empty constructs Geometries
+     * Constructs Geometries (empty)
      */
     public Geometries() {
     }
 
     /**
-     * parameters constructs Geometries
+     * Constructs Geometries with params
      * @param geometries group of shapes
      */
     public Geometries(Intersectable...geometries){
     }
 
+    /**
+     * Add shapes to the group
+     * @param geometries group of shapes
+     */
+    public void add(Intersectable... geometries){
+
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        return null;
+    }
 }
