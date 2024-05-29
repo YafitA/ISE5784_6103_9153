fc96e33ae2aa7563c885ea7fd3f8e770100adb1d
YafitA <YAFITTA5@GMAIL.COM>
2024-05-29 23:33:49 +0300

    shere tests finish

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -31,19 +31,39 @@ public class Sphere extends RadialGeometry {
 
     @Override
     public List<Point> findIntersections(Ray ray) {
+        //Ray starts at the center of sphere
+        if(ray.getHead().equals(this.center)) {
+            return List.of(ray.getPoint(this.radius));
+        }
         Vector u = this.center.subtract(ray.getHead());
         double tm = u.dotProduct(ray.getDirection());
-        double d = Math.sqrt(ray.getDirection().lengthSquared()- tm * tm);
-        if(d>=this.radius)
+
+        double d = Math.sqrt(u.lengthSquared()- tm * tm);
+        if(d>=this.radius)// No intersections
             return null;
         double th = Math.sqrt(this.radius*this.radius-d*d);
         double t1=tm+th;
         double t2=tm-th;
-        Point p1= this.center.add(ray.getDirection().scale(t1));
-        if(Util.alignZero(t2)<0){
-            return  List.of(p1);
+
+        boolean t1Valid = Util.alignZero(t1) > 0;
+        boolean t2Valid = Util.alignZero(t2) > 0;
+
+        if (t1Valid && t2Valid) {
+            // Both intersection points are valid
+            Point p1 = ray.getHead().add(ray.getDirection().scale(t1));
+            Point p2 = ray.getHead().add(ray.getDirection().scale(t2));
+            return List.of(p1, p2);
+        } else if (t1Valid) {
+            // Only the first intersection point is valid
+            Point p1 = ray.getHead().add(ray.getDirection().scale(t1));
+            return List.of(p1);
+        } else if (t2Valid) {
+            // Only the second intersection point is valid
+            Point p2 = ray.getHead().add(ray.getDirection().scale(t2));
+            return List.of(p2);
+        } else {
+            // No valid intersection points
+            return null;
         }
-        Point p2=this.center.add(ray.getDirection().scale(t2));
-        return List.of(p1,p2);
     }
 }
