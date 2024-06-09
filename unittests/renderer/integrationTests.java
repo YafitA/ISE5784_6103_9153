bae85fb3cf3e213df229f0625908d860523b7678
YafitA <YAFITTA5@GMAIL.COM>
2024-06-09 09:33:33 +0300

    integrationTests sphere and triangle done need plane

--- unittests/renderer/integrationTests.java
+++ unittests/renderer/integrationTests.java
@@ -1,7 +1,9 @@
 package renderer;
 
 import geometries.Intersectable;
+import geometries.Plane;
 import geometries.Sphere;
+import geometries.Triangle;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Ray;
@@ -25,6 +27,7 @@ public class integrationTests {
             .setVpDistance(1)
             .setVpSize(3,3);
 
+    private final Camera camera000=cameraBuilder.build();
     /**
      * Finds the number of Integration Points between camera and the shape
      * @param camera camera object
@@ -54,9 +57,8 @@ public class integrationTests {
     @Test
     void testIntegrationSphere() {
         //TC01: test case r=1 (2 points)
-        final Camera camera1=cameraBuilder.build();
         final Sphere sphere1=new Sphere(new Point(0,0,-3),1);
-        assertEquals(2,IntegrationPointsNumber(camera1,sphere1),
+        assertEquals(2,IntegrationPointsNumber(camera000,sphere1),
                 "wrong number of intersection with sphere");
 
         //TC02: test case r=2.5 (18 points)
@@ -65,7 +67,21 @@ public class integrationTests {
         assertEquals(18,IntegrationPointsNumber(camera2,sphere2),
                 "wrong number of intersection with sphere");
 
-        //97-98
+
+        //TC03: test case r=2 (10 points)
+        final Sphere sphere3=new Sphere(new Point(0,0,-2),2);
+        assertEquals(10,IntegrationPointsNumber(camera2,sphere3),
+                "wrong number of intersection with sphere");
+
+        //TC04: test case r=4 (9 points)
+        final Sphere sphere4=new Sphere(new Point(0,0,-0.5),4);
+        assertEquals(9,IntegrationPointsNumber(camera2,sphere4),
+                "wrong number of intersection with sphere");
+
+        //TC05: test case r=0.5 (0 point)
+        final Sphere sphere5=new Sphere(new Point(0,0,1),0.5);
+        assertEquals(0,IntegrationPointsNumber(camera2,sphere5),
+                "wrong number of intersection with sphere");
     }
 
     /**
@@ -73,7 +89,19 @@ public class integrationTests {
      * {@link renderer.Camera#constructRay(int, int, int, int)}.
      * {@link geometries.Triangle#findIntersections(primitives.Ray)}.
      */
+    @Test
     void testIntegrationTriangle() {
+        final Point p1=new Point(1,-1,-2);
+        final Point p2=new Point(-1,-1,-2);
+        //TC01: test case small triangle (1 point)
+        Triangle triangle1=new Triangle(new Point(0,1,-2),p1,p2);
+        assertEquals(1,IntegrationPointsNumber(camera000,triangle1),
+                "wrong number of intersection with triangle");
+
+        //TC02: test case big triangle (2 points)
+        Triangle triangle2=new Triangle(new Point(0,20,-2),p1,p2);
+        assertEquals(2,IntegrationPointsNumber(camera000,triangle2),
+                "wrong number of intersection with triangle");
     }
 
     /**
@@ -81,7 +109,23 @@ public class integrationTests {
      * {@link renderer.Camera#constructRay(int, int, int, int)}.
      * {@link geometries.Plane#findIntersections(primitives.Ray)}.
      */
+    @Test
     void testIntegrationPlane() {
+        //todo
+        //TC01: Plane is on vp (9 points)
+        final Plane Plane1=new Plane(new Point(10,0,-10),new Point(0,10,-10),new Point(0,-10,-10));
+        assertEquals(9,IntegrationPointsNumber(camera000,Plane1),
+                "wrong number of intersection with Plane");
+
+        //TC02: Plane cuts vp a small slope (9 points)
+        final Plane Plane2=new Plane(new Point(10,0,-10),new Point(0,10,-2),new Point(0,-10,-15));
+        assertEquals(9,IntegrationPointsNumber(camera000,Plane2),
+                "wrong number of intersection with Plane");
+
+        //TC03: Plane cuts vp a big slope (6 points)
+        final Plane Plane3=new Plane(new Point(10,0,-10),new Point(0,10,-2),new Point(0,-50,-50));
+        assertEquals(6,IntegrationPointsNumber(camera000,Plane3),
+                "wrong number of intersection with Plane");
     }
 
 }
